8fa4fd159060e8847e1855181dfa9ce679900ba5
Tyler <dkim477@aucklanduni.ac.nz>
2022-04-24 05:00:11 +1200

    start button implemented, needs to be debugged for it to work

---
+++ stopwatch.js
@@ -0,0 +1,33 @@
+const timeDisplay = document.querySelector("#timeDisplay");
+const startButton = document.querySelector("#startButton");
+const pauseButton = document.querySelector("#pauseButton");
+const resetButton = document.querySelector("#resetButton");
+
+let startTime = 0;
+let elapsedTime = 0;
+let currentTime = 0;
+let paused = true;
+let intervalId;
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+
+startButton.addEventListener("click", () => {
+    if (paused) {
+        paused = false;
+        startTime = Date.now() - elapsedTime;
+        intervalId = setInterval(updatetime, 1000);
+    }
+});
+pauseButton.addEventListener();
+resetButton.addEventListener();
+
+function updateTime() {
+    elapsedTime = Date.now() - startTime;
+
+    seconds = Math.floor((elapsedTime / 1000) % 60);
+    minutes = Math.floor((elapsedTime / 1000 * 60) % 60);
+    hours = Math.floor((elapsedTime / 1000 * 3600) % 60);
+
+    timeDisplay.textContent = '${hours}:${minutes}:${seconds}';
+}
