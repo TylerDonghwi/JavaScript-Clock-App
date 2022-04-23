319d78f85c7772b1ccc8bd47d2b69bfb8b8d6645
Tyler <dkim477@aucklanduni.ac.nz>
2022-04-24 05:24:18 +1200

    bug fixed, now it works fine

--- stopwatch.js
+++ stopwatch.js
@@ -16,7 +16,7 @@ startButton.addEventListener("click", () => {
     if (paused) {
         paused = false;
         startTime = Date.now() - elapsedTime;
-        intervalId = setInterval(updatetime, 75);
+        intervalId = setInterval(updateTime, 1000);
     }
 });
 pauseButton.addEventListener("click", () => {
@@ -29,27 +29,27 @@ pauseButton.addEventListener("click", () => {
 resetButton.addEventListener("click", () => {
     paused = true;
     clearInterval(intervalId);
-    let startTime = 0;
-    let elapsedTime = 0;
-    let currentTime = 0;
-    let hours = 0;
-    let minutes = 0;
-    let seconds = 0;
-    timeDisplay.textContent = "00:00:00"
+    startTime = 0;
+    elapsedTime = 0;
+    currentTime = 0;
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+    timeDisplay.textContent = "00:00:00";
 });
 
 function updateTime() {
     elapsedTime = Date.now() - startTime;
 
     seconds = Math.floor((elapsedTime / 1000) % 60);
-    minutes = Math.floor((elapsedTime / 1000 * 60) % 60);
-    hours = Math.floor((elapsedTime / 1000 * 3600) % 60);
-
-    timeDisplay.textContent = '${hours}:${minutes}:${seconds}';
+    minutes = Math.floor((elapsedTime / (1000 * 60)) % 60);
+    hours = Math.floor((elapsedTime / (1000 * 60 * 60)) % 60);
 
     seconds = pad(seconds);
     minutes = pad(minutes);
     hours = pad(hours);
+
+    timeDisplay.textContent = `${hours}:${minutes}:${seconds}`;
 
     function pad(unit) {
         return (("0") + unit).length > 2 ? unit : "0" + unit;
