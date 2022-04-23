1caeff9400955c244c81b927c191bca7aa162d3a
Tyler <dkim477@aucklanduni.ac.nz>
2022-04-24 05:14:43 +1200

    pause button and reset buttons added

--- stopwatch.js
+++ stopwatch.js
@@ -16,11 +16,27 @@ startButton.addEventListener("click", () => {
     if (paused) {
         paused = false;
         startTime = Date.now() - elapsedTime;
-        intervalId = setInterval(updatetime, 1000);
+        intervalId = setInterval(updatetime, 75);
     }
 });
-pauseButton.addEventListener();
-resetButton.addEventListener();
+pauseButton.addEventListener("click", () => {
+    if (!paused) {
+        paused = true;
+        elapsedTime = Date.now() - startTime;
+        clearInterval(intervalId);
+    }
+});
+resetButton.addEventListener("click", () => {
+    paused = true;
+    clearInterval(intervalId);
+    let startTime = 0;
+    let elapsedTime = 0;
+    let currentTime = 0;
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+    timeDisplay.textContent = "00:00:00"
+});
 
 function updateTime() {
     elapsedTime = Date.now() - startTime;
@@ -30,4 +46,12 @@ function updateTime() {
     hours = Math.floor((elapsedTime / 1000 * 3600) % 60);
 
     timeDisplay.textContent = '${hours}:${minutes}:${seconds}';
+
+    seconds = pad(seconds);
+    minutes = pad(minutes);
+    hours = pad(hours);
+
+    function pad(unit) {
+        return (("0") + unit).length > 2 ? unit : "0" + unit;
+    }
 }
